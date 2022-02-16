1e34cc367abccdd59fee149a2c21209a9a73aed7
Tanushree Gupta <tshreegupta@gmail.com>
2022-02-16 12:25:12 -0500

    Run script updated to make C code.
    Signed-off-by: Tanushree Gupta <tshreegupta@gmail.com>

--- demo/run.sh
+++ demo/run.sh
@@ -1,5 +1,11 @@
-echo "Generate Phantom cylinder"
+echo -e "Generate Phantom cylinder!\n"
 
 pushd ../python
 python3 create_ball.py
+popd
+
+
+echo -e "\nCompiling MBIR Helical code!\n"
+pushd ../src
+make
 popd 

7aeca96c0d2a0f9b7c2d34be3b8fb6edb3242ba0
XiaoWang <wangx2@ornl.gov>
2022-04-11 09:17:19 -0400

    add script for writing sino files

--- preprocess/singlegpu_test.py
+++ preprocess/singlegpu_test.py
@@ -29,16 +29,37 @@ NumRows=proj_info.Columns;
 NumChannels=proj_info.Rows;
 RescaleSlope = proj_info.RescaleSlope;
 RescaleIntercept = proj_info.RescaleIntercept;
-#proj_info = dicominfo('proj_0001.dcm',"dictionary",'C:/Users/xiaow/Downloads/AAPM-2022/DICOM-CT-PD-dict_v10.txt');
 
 #print("NumRows ",NumRows, " NumChannels ",NumChannels," RescaleSlope ",RescaleSlope," RescaleIntercept ",RescaleIntercept)
 
-focal1_Proj=torch.zeros(NumChannels,NumRows,NumViews).to(device)
+print(type(NumViews)," ",type(NumRows)," ",type(NumChannels))
+
+focal1_Proj=torch.zeros(NumChannels,NumRows,NumViews)
 for iv in range (0, NumViews):
     dataname=StringIO("/gpfs/alpine/med106/world-shared/irl1/aapm_data/dcmproj_copd/dcm_000/proj_%04d.dcm" % (iv+1))
     
-    print(dataname.getvalue())
+#    print(dataname.getvalue())
     temp = pydicom.dcmread(dataname.getvalue()).pixel_array
     focal1_Proj[:,:,iv] = torch.from_numpy(temp *RescaleSlope +RescaleIntercept)
 
 print("focal1_Proj max ",torch.max(focal1_Proj)," min ",torch.min(focal1_Proj), " dtype ",focal1_Proj.dtype)
+torch.permute(focal1_Proj, (1, 0, 2))
+
+weight = torch.exp(-0.3*focal1_Proj)
+
+
+dataname=StringIO("/gpfs/alpine/med106/world-shared/xf9/aapm-preprocess/dcm000_proj.sino")
+
+import struct
+with open(dataname.getvalue(),'w') as f:
+    f.write('%d ' % NumRows)
+    f.write('%d ' % NumChannels)
+    f.write('%d\n' % NumViews)
+
+#with open(dataname.getvalue(),'ab') as f:
+#    f.write(struct.pack("@f",focal1_Proj.cpu().numpy()))
+
+with open(dataname.getvalue(),'ab') as f:
+    np.array(focal1_Proj.cpu().numpy(),dtype=np.float32).tofile(f)
+
+

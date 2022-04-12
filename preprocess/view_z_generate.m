0d822124d311da02917ecfaead865e8b77bf3582
Xiao Wang <xiaowangatpurdue@gmail.com>
2022-04-12 13:02:56 -0700

    view and z generation matlab script for all datasets

--- preprocess/view_z_generate.m
+++ preprocess/view_z_generate.m
@@ -1,62 +1,51 @@
 clear all;
 
+for data_idx=0:199
+	if data_idx <67
+		dataset =sprintf('/global/cscratch1/sd/wang1698/AAPM_2022/raw/dcmproj_copd/dcm_%03d/',data_idx)
+	elseif data_idx >=67 && data_idx <134
+		dataset =sprintf('/global/cscratch1/sd/wang1698/AAPM_2022/raw/dcmproj_lung_lesion/dcm_%03d/',data_idx)
+	else
+		dataset =sprintf('/global/cscratch1/sd/wang1698/AAPM_2022/raw/dcmproj_liver/dcm_%03d/',data_idx)
+	end
 
-NumViews=9000;   
-FramesPerRotation = 1000;
+	a = dir([dataset,'*.dcm']);
+	NumViews = numel(a);
+	fprintf('NumViews is %d ',NumViews);
 
-%NumColumns is the number of channels. NumRows is the number of detector
-%rows
-rho_0_list=zeros(NumViews,1);
-z_center_list=zeros(NumViews,1);
-phi_0_list=zeros(NumViews,1);
 
-delta_rho_list=zeros(NumViews,1);
-delta_z_list=zeros(NumViews,1);
-delta_phi_list=zeros(NumViews,1);
+	z_center_list=zeros(NumViews,1);
+	output_view_angles=zeros(NumViews,1);
 
-for iv=1:NumViews
-    dataset=sprintf('C:/Users/xiaow/Downloads/AAPM-2022/dcmproj_copd/dcm_000/proj_%04d.dcm',iv);
-    temp_info = dicominfo(dataset,'dictionary','C:/Users/xiaow/Downloads/AAPM-2022/DICOM-CT-PD-dict_v10.txt');
-    rho_0_list(iv,1) = temp_info.DetectorFocalCenterRadialDistance;
-    delta_rho_list(iv,1)=0;
-    
-    
-    z_center_list(iv,1)=temp_info.DetectorFocalCenterAxialPosition;
-    delta_z_list(iv,1)=0;
-    
-    
-    phi_0_list(iv,1)=temp_info.DetectorFocalCenterAngularPosition;
-    delta_phi_list(iv,1)=0;
+
+	for iv=1:NumViews
+		if NumViews <10000	
+	    		proj=sprintf('proj_%04d.dcm',iv);
+		else
+			proj=sprintf('proj_%05d.dcm',iv);
+		end
+		proj_name =[dataset,proj];
+		temp_info = dicominfo(proj_name,'dictionary','./DICOM-CT-PD-dict_v10.txt');
+	    
+		z_center_list(iv,1)=temp_info.DetectorFocalCenterAxialPosition;
+	    
+	    	output_view_angles(iv,1)=temp_info.DetectorFocalCenterAngularPosition;
+		disp(iv)
+	end
+
+	for i=1:NumViews
+		output_view_angles(i,1) = output_view_angles(i,1)-pi*3/2;
+	end
+
+
+	output_path_view=sprintf('/global/cscratch1/sd/wang1698/AAPM_2022/preprocess/dcm%03d_viewAnglesList.txt',data_idx);
+
+	output_path_z=sprintf('/global/cscratch1/sd/wang1698/AAPM_2022/preprocess/dcm%03d_zPositionList.txt',data_idx);
+
+	dlmwrite(output_path_view,output_view_angles);
+	dlmwrite(output_path_z,z_center_list);
+
+
+	%dlmwrite('C:/Users/xiaow/Downloads/AAPM-2022/dcmproj_copd/dcm_000_preprocess/dcm000_viewAnglesList.txt',output_view_angles);
+	%dlmwrite('C:/Users/xiaow/Downloads/AAPM-2022/dcmproj_copd/dcm_000_preprocess/dcm000_zPositionList.txt',z_s_list);
 end
-
-x_center_list = - rho_0_list .* sin(phi_0_list);
-y_center_list = rho_0_list .* cos(phi_0_list);
-
-x_s_list = - (rho_0_list + delta_rho_list) .* sin(phi_0_list + delta_phi_list);
-y_s_list = (rho_0_list + + delta_rho_list) .* cos(phi_0_list + delta_phi_list);
-z_s_list = z_center_list + delta_z_list;
-
-
-
-%simulation now
-
-phi_0_simulated = zeros(NumViews,1);
-j = 0;
-for i=1:NumViews
-    if(i~=1 && phi_0_list(i,1)>phi_0_list(i-1,1))
-        j=j+1;
-    end
-        phi_0_simulated(i,1) = phi_0_list(i,1)-pi*3/2;
-end
-
-
-
-
-output_view_angles =zeros(NumViews,1);
-for i=1:NumViews
-    output_view_angles(i) = phi_0_simulated(i) + delta_phi_list(i);
-end
-
-dlmwrite('C:/Users/xiaow/Downloads/AAPM-2022/dcmproj_copd/dcm_000_preprocess/dcm000_viewAnglesList.txt',output_view_angles);
-dlmwrite('C:/Users/xiaow/Downloads/AAPM-2022/dcmproj_copd/dcm_000_preprocess/dcm000_zPositionList.txt',z_s_list);
-

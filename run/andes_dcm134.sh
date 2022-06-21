eafc8f439437b3baedb0c85c539fa488819e83ea
XiaoWang <wangx2@ornl.gov>
2022-06-21 14:52:23 -0400

    change output directory path

--- run/andes_dcm134.sh
+++ run/andes_dcm134.sh
@@ -2,9 +2,9 @@
 # FILENAME: submit.sh
 
 #SBATCH -N 4
-#SBATCH -t 01:00:00
+#SBATCH -t 04:00:00
 #SBATCH -J AAPM_134
-#SBATCH -A gen006
+#SBATCH -A gen150
 #SBATCH -p batch
 
 
@@ -33,7 +33,7 @@ forward_model_directory="../data/${weight_name}/forward_model_directory.txt"
 info_recon_directory="../data/${weight_name}/info_recon.txt"
 prior_directory="../data/${weight_name}/prior_qggmrf.txt"
 ce_directory="../data/${weight_name}/ce.txt"
-recon_directory="/gpfs/alpine/gen006/scratch/xf9/recon/dcm134/recon"
+recon_directory="/gpfs/alpine/gen006/proj-shared/xf9/recon/dcm134/recon"
 
 #srun -n $NUM_NODES -c 272 --cpu_bind=cores ../src/ct ${forward_model_directory} ${NUM_FOCAL_SPOTS} ${info_recon_directory} ${prior_directory} ${ce_directory} ${recon_directory} 70 ${DUAL_ENERGY} ${DEBUG_MODE} ${NUM_SOURCES}
 

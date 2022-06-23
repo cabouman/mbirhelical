734a2c3671ee4f641a0fb7ff76d3361fa2073068
XiaoWang <wangx2@ornl.gov>
2022-06-23 18:38:55 -0400

    add compiler export variables for integrating C code with tensor flow

--- Compile/andes_Compiler.sh
+++ Compile/andes_Compiler.sh
@@ -1,4 +1,11 @@
 module load gcc
 # for andes cluster
+
+export TF_DIR=/gpfs/alpine/proj-shared/gen006/muraligm/software/tensorflow_2_8_0_cpp
+export TF_INCLUDE_DIR=$TF_DIR/include
+export TF_LIBRARY_DIR=$TF_DIR/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$TF_LIBRARY_DIR
+
+
 cd ../src/
 make

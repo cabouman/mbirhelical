74c9d86dd6caaba71a734a3a7c653fe1bc7a1fa8
Murali Meena <gopalakrishm@ornl.gov>
2022-07-01 11:09:55 -0400

    For Murali's account

--- run/andes_dcm134.sh
+++ run/andes_dcm134.sh
@@ -2,9 +2,9 @@
 # FILENAME: submit.sh
 
 #SBATCH -N 4
-#SBATCH -t 00:30:00
+#SBATCH -t 04:00:00
 #SBATCH -J AAPM_134
-#SBATCH -A gen150
+#SBATCH -A stf006
 #SBATCH -p batch
 
 
@@ -32,11 +32,11 @@ forward_model_directory="../data/${weight_name}/forward_model_directory.txt"
 info_recon_directory="../data/${weight_name}/info_recon.txt"
 prior_directory="../data/${weight_name}/prior_dl.txt"
 ce_directory="../data/${weight_name}/ce.txt"
-recon_directory="/gpfs/alpine/gen006/proj-shared/xf9/recon/dcm134/recon"
+recon_directory="/gpfs/alpine/proj-shared/gen006/muraligm/AAPM_2022/TrueCT/recons/dcm134/recon"
 
 #srun -n $NUM_NODES -c 272 --cpu_bind=cores ../src/ct ${forward_model_directory} ${NUM_FOCAL_SPOTS} ${info_recon_directory} ${prior_directory} ${ce_directory} ${recon_directory} 70 ${DUAL_ENERGY} ${DEBUG_MODE} ${NUM_SOURCES}
 
-srun -N $NUM_NODES -n $NUM_NODES -c 32 --cpu_bind=cores ../src/ct ${forward_model_directory} ${NUM_FOCAL_SPOTS} ${info_recon_directory} ${prior_directory} ${ce_directory} ${recon_directory} 70 ${DUAL_ENERGY} ${DEBUG_MODE} ${NUM_SOURCES}
+srun -N $NUM_NODES -n $NUM_NODES -c $OMP_NUM_THREADS --cpu_bind=cores ../src/ct ${forward_model_directory} ${NUM_FOCAL_SPOTS} ${info_recon_directory} ${prior_directory} ${ce_directory} ${recon_directory} 70 ${DUAL_ENERGY} ${DEBUG_MODE} ${NUM_SOURCES}
 
 
 echo

b3e130f6b708b28df2c12c17879718c84cd12a28
gbuzzard <54102356+gbuzzard@users.noreply.github.com>
2022-09-19 13:23:41 -0400

    Change to create cone phantom.

--- python/create_ball.py
+++ python/create_ball.py
@@ -1,9 +1,11 @@
 import numpy as np
 import python.helical_io as hio
 
+
 # File to create a phantom with a single ball or cylinder, then write out relevant
 # information for projection.
-def create_cylinder(r, N):
+
+def create_cone(r, N):
     """
     Create an NxNxN array of floats, one constant value in a cylinder of given radius
     Args:
@@ -14,43 +16,41 @@ def create_cylinder(r, N):
         numpy array of floats of size NxNxN
         dictionary of image info
     """
-    radius = r
 
-    x0 = int(N/2)
-    y0 = int(N/2)
+    x0 = int(N / 2)
+    y0 = int(N / 2)
     # z0 = int(N/2) #zshift + 0.0
     Nx = N
-    Ny = N+1
-    Nz = N+2
-    img = 0.0*np.ones((Nx,Ny,Nz), dtype=np.float64)
+    Ny = N + 1
+    Nz = N + 2
+    img = 0.0 * np.ones((Nx, Ny, Nz), dtype=np.float32)
     _x = np.arange(img.shape[0])
     _y = np.arange(img.shape[1])
-    _z = np.arange(img.shape[2])
-    [X,Y,Z] = np.meshgrid(_x,_y,_z, indexing='ij') #N*N*N grid
+    [X, Y] = np.meshgrid(_x, _y, indexing='ij')  # N*N*N grid
 
-    dist_from_center = np.sqrt((X-x0)**2 + (Y-y0)**2)
+    dist_from_center = np.sqrt((X - x0) ** 2 + (Y - y0) ** 2)
 
-    mask = dist_from_center <=radius
-    img[mask] = 1.0
-    # TODO:  convert back to cylinder
-    img = np.arange(Nx*Ny*Nz).reshape((Nx, Ny, Nz))
-    #img = 1.0543108*np.ones((Nx, Ny, Nz))
+    for j in np.arange(Nz):
+        radius = r * (j + 1) / Nz
+        mask = dist_from_center <= radius
+        img[mask, j] = 1.0
+
     xc = 0.0
     yc = 0.0
-    zc = N/2
+    zc = N / 2
     Del_xy = 1.0
     Del_z = 1.0
 
-    res=[Del_xy, Del_xy, Del_z]
-    loc=[ xc-(N-1)*Del_xy/2,  yc-(N-1)*Del_xy/2, zc-(N-1)*Del_z/2 ]
-    #img=1000*img/(.02-.0000226)
+    res = [Del_xy, Del_xy, Del_z]
+    loc = [xc - (N - 1) * Del_xy / 2, yc - (N - 1) * Del_xy / 2, zc - (N - 1) * Del_z / 2]
+    # img=1000*img/(.02-.0000226)
     image_info = hio.create_image_info(img, res, loc)
     return img, image_info
 
 
 def create_ball(r, N):
     """
-    Create an NxNxN array of floats, one constant value in a cylinder of given radius
+    Create an NxNxN array of floats, one constant value in a ball of given radius
     Args:
         r:
         N:
@@ -61,41 +61,42 @@ def create_ball(r, N):
     radius = r
     # zshift = -0.5
 
-    x0 = int(N/2)
-    y0 = int(N/2)
-    z0 = int(N/2) #zshift + 0.0
-    img = 0.0002*np.ones((N,N,N))
+    x0 = int(N / 2)
+    y0 = int(N / 2)
+    z0 = int(N / 2)  # zshift + 0.0
+    img = 0.0002 * np.ones((N, N, N))
     _x = np.arange(N)
     _y = np.arange(N)
     _z = np.arange(N)
-    [X,Y,Z] = np.meshgrid(_x,_y,_z) #N*N*N grid
+    [X, Y, Z] = np.meshgrid(_x, _y, _z)  # N*N*N grid
 
-    dist_from_center = np.sqrt((X-x0)**2 + (Y-y0)**2 +(Z-z0)**2)
+    dist_from_center = np.sqrt((X - x0) ** 2 + (Y - y0) ** 2 + (Z - z0) ** 2)
 
-    mask = dist_from_center <=radius
+    mask = dist_from_center <= radius
     img[mask] = 0.06
 
     xc = 0.0
     yc = 0.0
-    zc = N/2
+    zc = N / 2
     Del_xy = 1.0
     Del_z = 1.0
 
-    res=[Del_xy, Del_xy, Del_z]
-    loc=[ xc-(N-1)*Del_xy/2,  yc-(N-1)*Del_xy/2, zc-(N-1)*Del_z/2 ]
-    img=1000*img/(.02-.0000226)
+    res = [Del_xy, Del_xy, Del_z]
+    loc = [xc - (N - 1) * Del_xy / 2, yc - (N - 1) * Del_xy / 2, zc - (N - 1) * Del_z / 2]
+    img = 1000 * img / (.02 - .0000226)
 
     image_info = hio.create_image_info(img, res, loc)
     return img, image_info
 
-if __name__=='__main__':
-    # fname = './data/phantom_ball.npy'  #numpy binary file
-    fname = 'cylinder_phantom_info.txt'  #numpy binary file
-    N = 10  #NxNxN phantom
-    r = 2
+
+if __name__ == '__main__':
+    base_name = 'cylinder_phantom'
+    fname = base_name + '_info.txt'
+    N = 100
+    r = 10
     # create_ball(fname,N)
-    image, img_info = create_cylinder(r, N)
-    phantom_fname = 'cylinder_phantom.raw'
-    hio.write_image_info(fname, img_info, phantom_fname)
-    hio.write_image(phantom_fname, img_info, image)
+    image, img_info = create_cone(r, N)
+
+    hio.write_image_info(fname, img_info, base_name)
+    hio.write_image(base_name, img_info, image)
     a = 0
--- python/helical_io.py
+++ python/helical_io.py
@@ -1,6 +1,6 @@
 import numpy as np
-import vtk
-from vtk.util import numpy_support
+# import vtk
+# from vtk.util import numpy_support
 
 """
 File to mimic the io operations in mbirhelical io.c 
@@ -8,7 +8,15 @@ File to mimic the io operations in mbirhelical io.c
 
 
 def create_image_info(image, res, loc):
+    """
+    Args:
+        image:
+        res:
+        loc:
 
+    Returns:
+
+    """
     image_info = dict()
     image_info['Nx'] = image.shape[0]
     image_info['Ny'] = image.shape[1]
@@ -31,11 +39,12 @@ def write_image_info(filename, img_info, image_file_name):
     Args:
         filename: file name to save info
         img_info: dict containing relevant image info
+        image_file_name: filename of image to be included in image_info (should have no extension)
 
     Returns:
         None
     """
-    img_info['imgFile'] = image_file_name
+    img_info['imgFile'] = image_file_name + '.mhimage'
     with open(filename, 'w') as fp:
         print("number of voxels in x", file=fp)
         print("%d\n" % img_info['Nx'], file=fp)
@@ -64,57 +73,74 @@ def write_image_info(filename, img_info, image_file_name):
 
 
 def write_image(filename, img_info, image):
-    with open(filename, 'w+b') as fp:
+    """
+    Save an image in two formats:
+        1. mhimage, which has 3 binary ints given the x, y, z dims, followed by data in 32-bit float form
+        2. raw, which has just the data in 32 bit form.  (This is mostly for debugging).
+        TODO: remove the raw format
+    Args:
+        filename: base name of file at which to store image - should have no extension
+        img_info: an image_info dict as returned by create_image_info
+        image: a 3D numpy array containing the image data
+
+    Returns:
+        None
+    """
+    with open(filename+'.mhimage', 'w+b') as fp:
         image_dims = np.array([img_info[s] for s in ['Nx', 'Ny', 'Nz']], dtype=int)
 
         # TODO: check for exact data type size and endianness to match c code.
         fp.write(image_dims.astype(np.int32).tobytes())
-        fp.write(image.astype(np.float64).tobytes())
+        fp.write(image.astype(np.float32).tobytes())
 
-def numpy_array_as_vtk_image_data(source_numpy_array):
-    """
-    :param source_numpy_array: source array with 2-3 dimensions. If used, the third dimension represents the channel count.
-    Note: Channels are flipped, i.e. source is assumed to be BGR instead of RGB (which works if you're using cv2.imread function to read three-channel images)
-    Note: Assumes array value at [0,0] represents the upper-left pixel.
-    :type source_numpy_array: np.ndarray
-    :return: vtk-compatible image, if conversion is successful. Raises exception otherwise
-    :rtype vtk.vtkImageData
-    """
+    with open(filename+'.raw', 'w+b') as fp:
+        fp.write(image.astype(np.float32).tobytes())
 
-    if len(source_numpy_array.shape) > 2:
-        channel_count = source_numpy_array.shape[2]
-    else:
-        channel_count = 1
 
-    output_vtk_image = vtk.vtkImageData()
-    output_vtk_image.SetDimensions(source_numpy_array.shape[1], source_numpy_array.shape[0], channel_count)
-
-    vtk_type_by_numpy_type = {
-        np.uint8: vtk.VTK_UNSIGNED_CHAR,
-        np.uint16: vtk.VTK_UNSIGNED_SHORT,
-        np.uint32: vtk.VTK_UNSIGNED_INT,
-        np.uint64: vtk.VTK_UNSIGNED_LONG if vtk.VTK_SIZEOF_LONG == 64 else vtk.VTK_UNSIGNED_LONG_LONG,
-        np.int8: vtk.VTK_CHAR,
-        np.int16: vtk.VTK_SHORT,
-        np.int32: vtk.VTK_INT,
-        np.int64: vtk.VTK_LONG if vtk.VTK_SIZEOF_LONG == 64 else vtk.VTK_LONG_LONG,
-        np.float32: vtk.VTK_FLOAT,
-        np.float64: vtk.VTK_DOUBLE
-    }
-    vtk_datatype = vtk_type_by_numpy_type[source_numpy_array.dtype.type]
-
-    source_numpy_array = np.flipud(source_numpy_array)
-
-    # Note: don't flip (take out next two lines) if input is RGB.
-    # Likewise, BGRA->RGBA would require a different reordering here.
-    if channel_count > 1:
-        source_numpy_array = np.flip(source_numpy_array, 2)
-
-    depth_array = numpy_support.numpy_to_vtk(source_numpy_array.ravel(), deep=True, array_type = vtk_datatype)
-    depth_array.SetNumberOfComponents(channel_count)
-    output_vtk_image.SetSpacing([1, 1, 1])
-    output_vtk_image.SetOrigin([-1, -1, -1])
-    output_vtk_image.GetPointData().SetScalars(depth_array)
-
-    output_vtk_image.Modified()
-    return output_vtk_image
+# def numpy_array_as_vtk_image_data(source_numpy_array):
+#     """
+#     :param source_numpy_array: source array with 2-3 dimensions. If used, the third dimension represents the channel count.
+#     Note: Channels are flipped, i.e. source is assumed to be BGR instead of RGB (which works if you're using cv2.imread function to read three-channel images)
+#     Note: Assumes array value at [0,0] represents the upper-left pixel.
+#     :type source_numpy_array: np.ndarray
+#     :return: vtk-compatible image, if conversion is successful. Raises exception otherwise
+#     :rtype vtk.vtkImageData
+#     """
+#
+#     if len(source_numpy_array.shape) > 2:
+#         channel_count = source_numpy_array.shape[2]
+#     else:
+#         channel_count = 1
+#
+#     output_vtk_image = vtk.vtkImageData()
+#     output_vtk_image.SetDimensions(source_numpy_array.shape[1], source_numpy_array.shape[0], channel_count)
+#
+#     vtk_type_by_numpy_type = {
+#         np.uint8: vtk.VTK_UNSIGNED_CHAR,
+#         np.uint16: vtk.VTK_UNSIGNED_SHORT,
+#         np.uint32: vtk.VTK_UNSIGNED_INT,
+#         np.uint64: vtk.VTK_UNSIGNED_LONG if vtk.VTK_SIZEOF_LONG == 64 else vtk.VTK_UNSIGNED_LONG_LONG,
+#         np.int8: vtk.VTK_CHAR,
+#         np.int16: vtk.VTK_SHORT,
+#         np.int32: vtk.VTK_INT,
+#         np.int64: vtk.VTK_LONG if vtk.VTK_SIZEOF_LONG == 64 else vtk.VTK_LONG_LONG,
+#         np.float32: vtk.VTK_FLOAT,
+#         np.float64: vtk.VTK_DOUBLE
+#     }
+#     vtk_datatype = vtk_type_by_numpy_type[source_numpy_array.dtype.type]
+#
+#     source_numpy_array = np.flipud(source_numpy_array)
+#
+#     # Note: don't flip (take out next two lines) if input is RGB.
+#     # Likewise, BGRA->RGBA would require a different reordering here.
+#     if channel_count > 1:
+#         source_numpy_array = np.flip(source_numpy_array, 2)
+#
+#     depth_array = numpy_support.numpy_to_vtk(source_numpy_array.ravel(), deep=True, array_type = vtk_datatype)
+#     depth_array.SetNumberOfComponents(channel_count)
+#     output_vtk_image.SetSpacing([1, 1, 1])
+#     output_vtk_image.SetOrigin([-1, -1, -1])
+#     output_vtk_image.GetPointData().SetScalars(depth_array)
+#
+#     output_vtk_image.Modified()
+#    return output_vtk_image
